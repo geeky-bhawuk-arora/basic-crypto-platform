8c227e4a325566b2f175e87e61259daea44b53be
Bhawuk Arora <bhawukarora042@gmail.com>
2024-07-04 22:28:41 +0530

    Updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/basic-crypto-platform/",
+  base: "/basic-crypto-platform/",
   plugins: [react()],
 })
